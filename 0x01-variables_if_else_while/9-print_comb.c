b9b0f9ee290c75ae5aad18fae104580c0a9c0e1c
Hossam Mahmoud <36197508+hossam-mahmoudatta@users.noreply.github.com>
2023-09-08 22:23:32 +0300

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,8 +16,11 @@ int main(void)
 	while (number < 10)
 	{
 		putchar('0' + number);
-		putchar(',');
-            	putchar(' ');
+		if (number < 9)
+		{
+			putchar(',');
+	            	putchar(' ');
+		}
 		number++;
 	}
 	putchar('\n');
