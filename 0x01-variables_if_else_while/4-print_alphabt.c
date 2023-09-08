0d5296ae4032b60b2612341cb1cda187db73c38b
Hossam Mahmoud <36197508+hossam-mahmoudatta@users.noreply.github.com>
2023-09-08 21:46:25 +0300

    Create 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - app start
+ *
+ * Return: 0 (Successfully executed)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' || letter != 'e')
+		{
+			putchar(letter);
+			letter++;
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
