bc1d59affc27ead01ff0cda5ee0d80f94ff4ee7d
Hossam Mahmoud <36197508+hossam-mahmoudatta@users.noreply.github.com>
2023-09-08 21:34:19 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,23 @@
-dd
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - app start
+ *
+ * Return: 0 (Successfully executed)
+ */
+int main(void)
+{
+  char letter = 'a';
+  
+  while (letter <= 'z') {
+    putchar(letter);
+    letter++;
+  }
+  
+  putchar('\n');
+  return 0;
+}
