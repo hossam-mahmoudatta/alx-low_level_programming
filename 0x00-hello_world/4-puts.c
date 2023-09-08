941a0eb6ee009311dd3691ae64f7b8aaafc3a4dd
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2023-09-07 18:09:04 -0700

    Added the puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,5 @@
-#!/bin/bash
-printf("Programming is like building a multilingual puzzle\n");
+#include <stdio.h>
+int main(void) {
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
