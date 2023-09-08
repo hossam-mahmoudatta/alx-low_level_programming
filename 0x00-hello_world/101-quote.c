557c23c1768569c23648a595200921efb067ec6a
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2023-09-07 18:42:33 -0700

    Added the 101-quote.c task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - application start
+ *
+ * Description: Prints text without usin printf or puts
+ *
+ * Return: 0 (Successfully)
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	return (1);
+}
