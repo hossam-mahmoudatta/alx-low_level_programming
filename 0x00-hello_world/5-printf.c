426a820edd648e26d327584904ea497a70ade431
hossam-mahmoudatta <hossam.uddin1@gmail.com>
2023-09-07 18:19:10 -0700

    Tweaked the printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main function - application start
+ *
+ * Return: 0 (Successfully)
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
