15b139ce7e80d365233e90084ca23a8f54182d43
Hossam Mahmoud <36197508+hossam-mahmoudatta@users.noreply.github.com>
2023-09-08 21:39:48 +0300

    Create 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - app start
+ *
+ * Return: 0 (Successfully executed)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+	return (0);
+}
