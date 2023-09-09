7d68aa1a06dd91d4f654860f1494e7e52607067b
Hossam Mahmoud <36197508+hossam-mahmoudatta@users.noreply.github.com>
2023-09-09 04:02:08 +0300

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -27,13 +27,10 @@ int main(void)
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(k + '0');
-					if (i < 9)
+					if (!(i == 7 && j == 8 && k == 9))
 					{
-						if (!(i == 7 && j == 8 && k == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
+						putchar(',');
+						putchar(' ');
 						prevDigit = i;
 					}
 				}
