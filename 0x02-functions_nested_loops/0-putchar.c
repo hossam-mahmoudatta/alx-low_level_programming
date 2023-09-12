714dce3220ecae3f92f1d888ecc264cb384f4451
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 02:31:03 +0300

    Uploading task 0.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - app start
+ *
+ * Return: 0 (Successfully executed)
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+
+	putchar('\n');
+	return (0);
+}
