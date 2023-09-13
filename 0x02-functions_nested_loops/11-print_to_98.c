30928f54f9b7a114691859c790e46ed605deb1c1
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 05:38:56 +0300

    dsd

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,11 +18,11 @@ void print_to_98(int n)
 	{
 		for (i = n ; i > 97 ; i--)
 		{
-			if (n != 98)
+			if (i != 98)
 			{
 				printf("%d, ", i);
 			}
-			else if (n == 98)
+			else if (i == 98)
 			{
 				printf("%d\n", i);
 			}
@@ -36,11 +36,11 @@ void print_to_98(int n)
 	{
 		for (i = n ; i < 99 ; i++)
 		{
-			if (n != 98)
+			if (i != 98)
 			{
 				printf("%d, ", i);
 			}
-			else if (n == 98)
+			else if (i == 98)
 			{
 				printf("%d\n", i);
 			}
