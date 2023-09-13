19ece09b9dd3391213f6c52d9c6c55dcee11181d
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 05:20:32 +0300

    Uploading the file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_to_98 - prints the 9 times table
+ *
+ * @n: 1st parameter
+ *
+ * Return: result
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = 0 ; i < 99 ; i++)
+	{
+		_putchar(i + '0');
+		_putchar(',');
+		_putchar(' ');
+	}
+}
