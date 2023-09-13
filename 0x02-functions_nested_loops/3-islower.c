fba9ef3c57796d3c525fe6655295b4c9206c52eb
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 03:33:31 +0300

    created file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * _islower - checks a lowercase character
+ *
+ * Return: 0 (otherwise)
+ */
+
+int _islower(int c)
+{
+	if ((c > 96) && (c < 123))
+    {
+        return(1);
+    }
+    else
+    {
+        return(0);
+    }
+
+}
