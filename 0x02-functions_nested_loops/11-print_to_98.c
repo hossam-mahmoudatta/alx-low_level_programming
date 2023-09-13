c5feeeca396a421451dcb9d005e4c944d75d7836
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 05:29:50 +0300

    changed how it outputs

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -23,18 +23,18 @@ void print_to_98(int n)
 	{
 		for (i = n ; i > 97 ; i--)
 		{
-			printf("%d\n", i);
+			printf("%d, ", i);
 		}
 	}
 	else if (n == 98)
 	{
-			printf("%d\n", n);
+			printf("%d, ", n);
 	}
 	else if (n < 98)
 	{
 		for (i = n ; i < 99 ; i++)
 		{
-			printf("%d\n", i);
+			printf("%d, ", i);
 		}
 	}
 }
