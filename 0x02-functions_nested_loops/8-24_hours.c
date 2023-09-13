49c5004f0e997de3ba323be19df94db104c20b60
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 05:01:08 +0300

    changed the cocde again

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,9 +16,11 @@ void jack_bauer(void)
 	{
 		for (j = 0 ; j < 60 ; j++)
 		{
-			_putchar('0' + i);
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar('0' + j);
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 		}
 	}
