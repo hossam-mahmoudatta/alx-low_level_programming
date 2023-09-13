0886f0739637059af84a2583a411fb339e428115
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 04:51:16 +0300

    Added new task

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * jack_bauer - prints every min of jack
+ *
+ * Return: 0 (otherwise)
+ */
+
+ void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for(i = 0 ; i < 24 ; i++)
+	{
+		_putchar(i + '0');
+		_putchar(':');
+		for(j = 0 ; j < 60 ; j++)
+		{
+			_putchar(j + '0');
+		}
+		_putchar('\n');
+	}
+}
