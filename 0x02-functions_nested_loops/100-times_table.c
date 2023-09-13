08bece00bdb47b11d668748811f53ecd63901317
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 05:47:12 +0300

    dsa

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,11 +13,12 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-	if ((n > 0) && (n < 16))
+
+	if ((n >= 0) && (n < 16))
 	{
-		for (i = 0 ; i < n ; i++)
+		for (i = 0 ; i <= n ; i++)
 		{
-			for (j = 0 ; j < n ; j++)
+			for (j = 0 ; j <= n ; j++)
 			{
 				k = j * i;
 				if (j == 0)
