97b1e64f5fef5ef130f25164e51ff8282225ea92
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 03:52:30 +0300

    added description

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,6 +5,8 @@
 /**
  * _islower - checks a lowercase character
  *
+ * @c: letter being tested
+ * 
  * Return: 0 (otherwise)
  */
 
@@ -12,10 +14,10 @@ int _islower(int c)
 {
 	if ((c > 96) && (c < 123))
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
