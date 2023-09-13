1f782cc1e8db03f84fe173a9becce86124b9a49b
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 03:21:31 +0300

    Creating new task 2.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_alphabet - prints the alphabet from a to z
+ *
+ * Return: 0 (Successfully executed)
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (letter <= 'z')
+		{
+			putchar(letter);
+			letter++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
