8c2805a0c47901dd0d7783d5f4ea3b69999ef174
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 04:40:26 +0300

    tweaked code again

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -14,11 +14,11 @@ int print_last_digit(int n)
 {
 	int digit;
 	
-	if (n < 0)
-	{
-		n = -n;
-	}
 	digit = n % 10;
+	if (digit < 0)
+	{
+		digit = -1 * digit;
+	}
 	_putchar (digit + '0');
 
 	return (digit);
