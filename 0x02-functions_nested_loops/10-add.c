437cae3dc245d91c331a379d2124bc49b48c6020
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 05:14:28 +0300

    upload code for the first time

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: 0 (otherwise)
+ */
+
+int add(int m, int n)
+{
+	return (m + n);
+}
