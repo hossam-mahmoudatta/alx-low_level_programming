b8129c37a3dd2d4caed80c780b46e554d652f292
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-13 05:32:53 +0300

    changed again the output format

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,16 +14,14 @@ void print_to_98(int n)
 {
 	int i;
 
-	if (n < 0)
-	{
-		n = n * -1;
-	}
-
 	if (n > 98)
 	{
 		for (i = n ; i > 97 ; i--)
 		{
-			printf("%d, ", i);
+			if (n != 98)
+			{
+				printf("%d, ", i);
+			}
 		}
 	}
 	else if (n == 98)
@@ -34,7 +32,10 @@ void print_to_98(int n)
 	{
 		for (i = n ; i < 99 ; i++)
 		{
-			printf("%d, ", i);
+			if (n != 98)
+			{
+				printf("%d, ", i);
+			}
 		}
 	}
 }
