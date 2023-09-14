564f1f2a588e5853779fd3c37b484b25a3440263
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-15 02:19:04 +0300

    added file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,8 +5,10 @@
 #include <time.h>
 #include <stdio.h>
 
+extern int _putchar(char c);
 extern int _isupper(int c);
 extern int _isdigit(int c);
 extern int mul(int a, int b);
+void print_numbers(void);
 
 #endif
