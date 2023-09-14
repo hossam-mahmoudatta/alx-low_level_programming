2a093ad4a649e8b920a7957a20ae3022667bca9b
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-15 02:13:21 +0300

    adding new project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * mul - multiplies two values
+ *
+ * @a: letter being tested
+ * @b: letter being tested
+ *
+ * Return: 1 (otherwise)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,6 @@
 
 extern int _isupper(int c);
 extern int _isdigit(int c);
+extern int mul(int a, int b);
 
 #endif
