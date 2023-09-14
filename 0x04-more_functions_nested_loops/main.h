ea503a159226bee584cb62ca7f1abe4945006803
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-15 01:44:49 +0300

    das

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - app start
+ *
+ * Return: 0 (Successfully executed)
+ */
+
+int _isupper(int c);
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int m, int n);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
