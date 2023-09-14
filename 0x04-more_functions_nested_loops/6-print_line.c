fd3166c595a8c5d759520f6afe11490a2d31d47d
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-15 02:59:58 +0300

    added task 6.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_line - prints a line
+ *
+ * @n: letter being tested
+ * 
+ * Return: Nothing
+ */
+
+void print_line(int n)
+{
+	int i = 0;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		if (n < 0)
+		{
+			putchar('\n');
+		}
+		else
+		{
+			putchar('_');
+		}
+	}
+	putchar('\n');
+}
