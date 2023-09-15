e6aad56ea3ceecd686ce1e780b96b303cbe93479
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-15 03:33:14 +0300

    task 9.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - prints a square
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	int col;
+
+	for (i = 1 ; i < 101 ; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%s", "Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", "Buzz");
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s", "FizzBuzz");
+		}
+		printf("%d", i);
+	}
+	printf("\n");
+}
