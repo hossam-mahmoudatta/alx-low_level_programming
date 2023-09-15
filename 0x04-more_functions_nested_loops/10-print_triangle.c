10b7b9a21edc6864bde9eda3ff4d10741539208a
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-15 04:13:03 +0300

    added task 10.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_triangle - prints number multiple times
+ * 
+ * @size: letter being tested
+ *
+ * Return: Nothing
+ */
+
+void print_triangle(int size)
+{
+	int i = 0;
+	int j, size = 5;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = i; j < size ; j++)
+		{
+			putchar(' ');
+		}
+		for (j = 0; j < i; j++)
+		{
+			putchar('#');
+		}
+		putchar('\n');
+	}
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+}
