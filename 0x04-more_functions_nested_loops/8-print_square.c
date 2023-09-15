1b223ef17718f3e077e6028c7db3d37b39bdce6a
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-15 03:25:08 +0300

    added task 8.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * print_diagonal - prints a line
+ *
+ * @size: letter being tested
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	int col;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		for (col = 0 ; col < size ; col++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
