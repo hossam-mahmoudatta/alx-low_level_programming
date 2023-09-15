5c36d1605793e40074c53fa029493ca4381285d8
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-15 04:17:00 +0300

    das

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,21 +15,23 @@ void print_triangle(int size)
 	int i = 0;
 	int j = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = i ; j < size ; j++)
-		{
-			putchar(' ');
-		}
-		for (j = 0; j < i; j++)
-		{
-			putchar('#');
-		}
-		putchar('\n');
-	}
-
 	if (size <= 0)
 	{
 		putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = i ; j < size ; j++)
+			{
+				putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
 	}
 }
