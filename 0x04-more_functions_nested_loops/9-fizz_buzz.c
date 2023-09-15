5136c7f378a5a41fcf039bcd5bd7f585e3c42e4b
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-15 04:03:04 +0300

    change sequence of condition

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,17 +14,17 @@ int main(void)
 
 	for (i = 1 ; i < 101 ; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 15 == 0)
+		{
+			printf("%s", "FizzBuzz");
+		}
+		else if (i % 3 == 0)
 		{
 			printf("%s", "Fizz");
 		}
 		else if (i % 5 == 0)
 		{
 			printf("%s", "Buzz");
-		}
-		else if ((i % 3 == 0) && (i % 5 == 0))
-		{
-			printf("%s", "FizzBuzz");
 		}
 		else
 		{
