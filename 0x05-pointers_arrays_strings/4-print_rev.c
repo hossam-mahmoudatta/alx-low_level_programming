928013d17894c424f675bcaea5d0d6f9c652b3f1
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-18 15:16:19 +0300

    adding task 4.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - a function that prints a string
+ *
+ * @str: the tested variable
+ *
+ * Return: None.
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (i = length -1 ; i >= 0 ; i--)
+	{
+		printf("%c", s[i]);	
+	}
+	printf("\n");
+}
