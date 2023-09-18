276759416f195313290a4b7ac63611ba494bfcf8
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-18 16:43:20 +0300

    updating condition

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,9 +15,10 @@ void print_array(int *a, int n)
 
 	for (i = 0 ; i < n ; i++)
 	{
-		if (i != n)
+		printf("%d", a[i]);
+		if (i < n - 1)
 		{
-			printf("%d, ", a[i]);
+			printf(", ");
 		}
 	}
 	printf("\n");
