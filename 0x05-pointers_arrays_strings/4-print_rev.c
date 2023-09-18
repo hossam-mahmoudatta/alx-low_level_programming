850636aff9cd8d4c4bd38d049f4a62e25ffb3e61
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-18 15:17:14 +0300

    correcting betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,9 +17,9 @@ void print_rev(char *s)
 	{
 		length++;
 	}
-	for (i = length -1 ; i >= 0 ; i--)
+	for (i = length - 1 ; i >= 0 ; i--)
 	{
-		printf("%c", s[i]);	
+		printf("%c", s[i]);
 	}
 	printf("\n");
 }
