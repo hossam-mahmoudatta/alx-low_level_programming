75a236111dc233e9b759bb600bac1bd467a2993a
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-18 15:33:54 +0300

    commit the algo

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@
 void rev_string(char *s)
 {
 	int length = 0;
-    char *temp = 0;
+	char *temp;
 	int i;
 
 	while (s[length] != '\0')
@@ -20,11 +20,11 @@ void rev_string(char *s)
 	}
 	for (i = length - 1 ; i >= 0 ; i--)
 	{
-		temp[i] = s[i]; 
+		temp[i] = s[i];
 	}
 	for (i = 0 ; i < length - 1 ; i++)
 	{
-		s[i] = temp[i]; 
+		s[i] = temp[i];
 	}
 	printf("\n");
 }
