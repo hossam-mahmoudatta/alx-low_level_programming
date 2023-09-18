0354cd9d336b8d7aeb10d716ec3ae5fc98d9ec92
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-18 13:40:38 +0300

    updating betty codestyle

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,7 +2,7 @@
 
 /**
  * swap_int - a function that swaps two vars
- * 
+ *
  * @a: the tested variable
  * @b: the tested variable
  *
@@ -12,6 +12,7 @@
 void swap_int(int *a, int *b)
 {
 	int temp;
+	
 	temp = *a;
 	*a = *b;
 	*b = temp;
