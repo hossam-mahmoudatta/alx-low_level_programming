e1b1b7054d0c0feb138343e5699b8813df5993ff
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-18 23:05:41 +0300

    chnaging betty style

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,12 +10,13 @@
 
 int _atoi(char *s)
 {
-	int length = 0;
 	int i = 0;
 	int sign = 1;
 	unsigned int result = 0;
 
-	// This block checks if there is polarity
+	/**
+	 * This block checks the polarity of the number
+	 */
 	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
 		if (s[i] == '-')
