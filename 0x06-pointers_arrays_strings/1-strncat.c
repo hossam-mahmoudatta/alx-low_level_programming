b89be74ce3a12a8c1a712abe7b939adad5eec9df
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-20 23:04:28 +0300

    uploaded task 1.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that appends two strings
+ *
+ * @dest: the tested variable
+ * @src: the tested variable
+ * @n: the tested variable
+ *
+ * Return: string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int length = 0;
+	int i = 0;
+
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[length] = src[i];
+		i++;
+		length++;
+	}
+	dest[length] = '\0';
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@
 
 extern int _putchar(char c);
 char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
