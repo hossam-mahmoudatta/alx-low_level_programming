2031f516258c08c6b960e806d43494ad3027a230
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-26 02:06:08 +0300

    changing algo

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,19 +14,18 @@ char *_strchr(char *s, char c)
 	int i = 0;
 	int k = 0;
 
-	while (s[i] != c)
+	while (*s != '\0')
 	{
-		i++;
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
 	}
 
-	char* output = (char*)malloc (strlen(s) - i);
-
-	while (s[i] != '\0')
-	{
-		output[k] = s[i];
-		i++;
-		k++;
-	}
-
-	return (output);
+	return (0);
 }
