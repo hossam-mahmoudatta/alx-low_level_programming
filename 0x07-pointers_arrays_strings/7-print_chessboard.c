103d4ae0c4ccf5b2c9fb0655041c92c3b4e30a3a
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-26 04:40:51 +0300

    upload remaining tasks

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints the chess board
+ *
+ * @a[8]: the tested variable
+ *
+ * Return: Void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+    int row = 0;
+	int col = 0;
+
+	for (row = 0 ; row < 8 ; row++)
+	{
+		for (col = 0 ; col < 8 ; col++)
+		{
+			_putchar(*(*(row + a) + col));
+		}
+		_putchar('\n');
+	}
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,6 +13,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
