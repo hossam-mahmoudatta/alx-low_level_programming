2a89427eec5a27173f42588aad24f67ed43a3037
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-09-29 02:52:40 +0300

    das

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * is_prime_number - func that returns 1 if i/p is prime
+ *
+ * @n: the tested variable
+ *
+ * Return: Integer
+ */
+
+int is_palindrome(char *s)
+{
+	if (s < 0)
+	{
+		return (-1);
+	}
+
+	if (s == 0)
+	{
+		return (1);
+	}
+
+	return (s);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,6 +14,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
