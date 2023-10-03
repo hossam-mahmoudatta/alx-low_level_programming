750ceae7262757fc16e33688103df235e3e8b675
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-03 05:31:57 +0300

    changing code

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,13 +2,15 @@
 
 /**
  * main - Prints the program name, followed by a new line.
+ *
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: Always o.
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char const *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
