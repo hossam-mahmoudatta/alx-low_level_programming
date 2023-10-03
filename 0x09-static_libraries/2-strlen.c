9efc1d0d0913de0cce19856cadfb1512455b84e1
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-03 05:11:41 +0300

    uploading new files

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that prints string length
+ *
+ * @s: the tested variable
+ *
+ * Return: None.
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return (length);
+}
