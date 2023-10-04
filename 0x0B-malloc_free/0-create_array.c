3244836677f139ace4d8f4aa28e2e382b3c935e2
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-05 01:35:20 +0300

    uploading new task for this project

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat - a function that appends two strings
+ *
+ * @dest: the tested variable
+ * @src: the tested variable
+ *
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int length = 0;
+	int i = 0;
+
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+
+	while (src[i] != '\0')
+	{
+		dest[length] = src[i];
+		i++;
+		length++;
+	}
+	dest[length] = '\0';
+
+	return (dest);
+}
