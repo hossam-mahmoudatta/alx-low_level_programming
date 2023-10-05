35c73e26401d30ad2869e428e68060e08d53c3da
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-05 16:07:53 +0300

    betty

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -40,7 +40,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0 ; i < ac ; i++)
 	{
-		for ( j = 0 ; av[i][j] != '\0' ; j++)
+		for (j = 0 ; av[i][j] != '\0' ; j++)
 			argumentConcat[argIndex++] = av[i][j];
 
 		if (i < ac - 1)
