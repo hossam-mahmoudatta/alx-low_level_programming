5d81a5dc44a31dd2b4a818df4f2957526032df04
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-05 16:00:22 +0300

    uploading adv task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * argstostr - function that concatenates all the 
+ * arguments of your program.
+ *
+ * @ac: the tested variable
+ * @av: the tested variable
+ *
+ * Return: string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *argumentConcat;
+	int i, j, k, argIndex = 0;
+	int totalLength = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0 ; i < ac ; i++)
+	{
+		if (av[i] == NULL)
+			return (NULL);
+
+		for (j = 0 ; av[i][j] != '\0' ; j++)
+			totalLength++;
+
+		totalLength++;
+	}
+
+	argumentConcat = malloc((totalLength + 1) * sizeof(char));
+
+	if (argumentConcat == NULL)
+	{
+		free(argumentConcat);
+		return (NULL);
+	}
+
+	for (i = 0 ; i < ac ; i++)
+	{
+		for (int j = 0 ; av[i][j] != '\0' ; j++)
+            argumentConcat[argIndex++] = av[i][j];
+		
+        if (i < ac - 1)
+            argumentConcat[argIndex++] = ' ';
+	}
+
+	argumentConcat[argIndex] = '\0';
+
+	return (argumentConcat);
+}
