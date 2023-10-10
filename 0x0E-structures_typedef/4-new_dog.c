39536c4d55927e6fdb5b0e781331dec8a94f9ad1
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-11 02:39:54 +0300

    das

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * new_dog - Prints all the data of a dog
+ * 
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Return: Nothing
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	printf("Name: %s\n", (d -> name) ? d -> name : "(nil)");
+	printf("Age: %f\n", (d -> age) ? d -> age : 0);
+	printf("Owner: %s\n", (d -> owner) ? d->owner : "(nil)");
+}
