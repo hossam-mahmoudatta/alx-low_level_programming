48d828341ce0b0ee8064ec25ec1479130ead112b
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-11 02:41:00 +0300

    das

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,6 +12,8 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *d;
+
 	printf("Name: %s\n", (d -> name) ? d -> name : "(nil)");
 	printf("Age: %f\n", (d -> age) ? d -> age : 0);
 	printf("Owner: %s\n", (d -> owner) ? d->owner : "(nil)");
