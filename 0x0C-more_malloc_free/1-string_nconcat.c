d30bd03650924e832d1bee0c89c87efe0153ae15
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-10 03:58:06 +0300

    das

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  * Return: string
  */
 
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char* string_nconcat(const char* s1, const char* s2, unsigned int n)
 {	
 	unsigned int i, j;
 	char *memAllocate;
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,7 +9,7 @@
 int _putchar(char c);
 int _strlen(char *string);
 void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
+char* string_nconcat(const char* s1, const char* s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
