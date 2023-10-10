0774c72dfc55116d0b9619f4ae00e0e274ac7a25
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-10 03:29:09 +0300

    das

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,11 +12,12 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {	
 	unsigned int i;
+	char *memAllocate;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	char *memAllocate = malloc(nmemb * size);
+	memAllocate = malloc(nmemb * size);
 
 	if (memAllocate == NULL)
 		return (NULL);
