78c200d9ed7b2f02a32a6fa52f7b9a918bf5c465
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-12 01:13:06 +0300

    das

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_name - preints the name of the file
  * 
  * @name The tested variable.
- * @s2: The tested variable.
- * @n: The tested variable.
+ * @f: The tested variable.
  *
  * Return: Always 0 (Success)
  */
 void print_name(char *name, void (*f)(char *))
 {
 	if (f != NULL)
-	f(name);
+		f(name);
 }
