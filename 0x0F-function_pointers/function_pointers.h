a05b318fb455a434634f42437b649425ac9adebf
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-14 00:39:49 +0300

    das

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _FUNCTION_POINTERS_H
+#define _FUNCTION_POINTERS_H
 
 #include <stdlib.h>
 #include <time.h>
@@ -10,12 +10,5 @@ int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
-int _strlen(char *string);
-void *malloc_checked(unsigned int b);
-char *string_nconcat(const char *s1, const char *s2, unsigned int n);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
