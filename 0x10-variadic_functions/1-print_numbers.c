3bceaae52fb72f919cf47e6f3afec04ca2ded220
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-13 05:29:08 +0300

    qwa

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,10 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0 ; i < n ; i++)
 	{
-		printf('%d', va_arg(argsHolder, int));
+		printf("%d", va_arg(argsHolder, int));
 
 		if (separator != NULL)
-			printf('%s', separator);
+			printf("%s", separator);
 	}
 
 	putchar("\n");
