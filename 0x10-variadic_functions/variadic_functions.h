028660211145d03af436169b3993e71a0a70727e
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-13 16:01:05 +0300

    betty

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -12,9 +12,8 @@ int _strlen(char *string);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-
 void *malloc_checked(unsigned int b);
-char* string_nconcat(const char *s1, const char *s2, unsigned int n);
+char *string_nconcat(const char *s1, const char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
