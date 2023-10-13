e3055ee6a769893b9cf26e3380ca022c55be5392
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-14 00:36:33 +0300

    das

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,34 +1,37 @@
-#include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include "function_pointers.h"
 
 /**
- * main - the main function
+ * main - Write a program that prints the opcodes of its own main function
  *
  * @argc: number of input args
  * @argv: the content of those argc
  *
- * Return: integer
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	char *p = (char *)main;
+	int value;
 
-	if (argc != 4)
+	if (argc != 2)
 	{
 		printf("Error\n");
-		exit(98);
+		exit(1);
 	}
 
-	oprt = get_op_func(argv[2]);
+	value = atoi(argv[1]);
 
-	if (!oprt)
+	if (value < 0)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(2);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	while (value--)
+	{
+		printf("02hhx%s", *p++, value ? " " : "\n");
+	}
+
 	return (0);
 }
