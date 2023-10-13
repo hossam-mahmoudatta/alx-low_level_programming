b88df0e109aca8b3f2e5462d22b9340c3f8e9b5b
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-13 05:10:32 +0300

    DAS

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -16,9 +16,6 @@ int sum_them_all(const unsigned int n, ...)
 	va_list argsHolder;
 	va_start(argsHolder, n);
 
-	if ( n == 0)
-		return (0);
-
 	for (i = 0 ; i < n ; i++)
 		sum += va_arg(argsHolder, int);
 
