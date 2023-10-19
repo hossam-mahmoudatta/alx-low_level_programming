7b7f075f14c8ffef8123b72d26aad3f7b6bda792
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-19 23:36:45 +0300

    das

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,10 +12,20 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
+	if (!h)
+		return (0);
+
 	while (h)
 	{
 		if (!h->str)
+		{
 			printf("[0] (nil)\n");
+		}
+
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 
 		h = h->next;
 		n++;
