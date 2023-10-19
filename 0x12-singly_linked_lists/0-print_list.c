3441a640149bf0934724ff4f4ce9230decf58d7a
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-19 23:13:03 +0300

    updating the algo

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,17 @@
 
 size_t print_list(const list_t *h)
 {
-	if (h->str == NULL)
-		printf("(nil)");
+	size_t n = 0;
 
-	while (h != NULL)
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+
+	else
 	{
-		printf("%d -> ", h->len);
+		printf("[%u] %s\n ", h->len, h->str);
 		h = h->next;
+		n++;
 	}
-	printf("NULL\n");
+
+	return (n);
 }
