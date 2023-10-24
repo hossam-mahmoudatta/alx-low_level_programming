97dfd1467dac1f363c774013b212b6ff6eb94c44
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-25 00:49:45 +0300

    das

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,14 +12,12 @@ void free_listint2(listint_t **head);
 {
 	listint_t *temp;
 
-	temp = head;
-
 	if (head == NULL)
 		return;
-	while (head != NULL)
+	while (*head != NULL)
 	{
 		temp = *(head)->next;
 		free(*head);
-		*head = *temp;
+		*head = temp;
 	}
 }
