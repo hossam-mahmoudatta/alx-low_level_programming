ff0961e546ce2e2289db05c715b24bde7a0e3b0c
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-25 00:47:54 +0300

    das

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint2 - that adds a new node at the end of a list
+ * pop_listint - that adds a new node at the end of a list
  *
  * @head: the struct that contains the data
  *
