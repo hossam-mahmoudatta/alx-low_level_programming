039ed6a25d2932c2e81f1d5ebf2144fbc5df412b
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-25 00:02:11 +0300

    das

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,13 +21,8 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+size_t print_listint(const listint_t *h);
 
-void __attribute__ ((constructor)) execB4Main();
 
 int _putchar(char c);
 
