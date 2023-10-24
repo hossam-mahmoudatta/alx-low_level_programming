3f4cf5a59f7c4e8407cf3530bd4713af225c429d
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-25 00:27:11 +0300

    das

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,8 +11,8 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	
 	listint_t *node;
+
 	node = malloc(sizeof(listint_t));
 
 	if (!node)
@@ -23,5 +23,5 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	node->n = n;
 	node->next = *head;
 	*head = node;
-	 return (*head);
+	return (*head);
 }
