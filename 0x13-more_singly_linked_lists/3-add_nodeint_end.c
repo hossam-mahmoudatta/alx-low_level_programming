824b4edc0917c78c316a6d449978684416f4b070
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-25 00:23:35 +0300

    das

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,24 +11,16 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	list_t *node = malloc(sizeof(list_t));
-	list_t *tempHead;
+	listint_t *node = malloc(sizeof(listint_t));
+	listint_t *tempHead;
 
 	if (!node)
 	{
 		return (NULL);
 	}
 
-	node->str = strdup(str);
-
-	if (!node->str)
-	{
-		free(node);
-		return (NULL);
-	}
-
-	node->len = strlen(str);
-	tempHead = *head;
+	node->n = n;
+	node->next = NULL;
 
 	if (!*head)
 	{
@@ -36,8 +28,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (node);
 	}
 
+	tempHead = *head;
 	while (tempHead->next)
+	{
 		tempHead = tempHead->next;
+	}
 
 	tempHead->next = node;
 
