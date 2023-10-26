12185b799772747513dc4c9e9747d4488be4ea38
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-10-27 01:15:06 +0200

    ads

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * clear_bit - prints all elements of a linked list
+ *
+ * @n: the struct that contains the data
+ * @index: the struct that contains the data
+ *
+ * Return: no. of nodes printed
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int i, FlipsNumber = 0;
+	unsigned long int j = sizeof(unsigned long int) * 8;
+
+	for (i = 0 ; i < j ; i++)
+	{
+		if ((m & 1) != (n & 1))
+			FlipsNumber += 1;
+		n = n >> 1;
+		m = m >> 1;
+	}
+	return (FlipsNumber);
+}
