82c1eea6a1d1c923d0171cf4477c0f5e5fb54757
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-11-16 19:44:15 +0200

    the betty styling in the function

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,42 +11,31 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fileDescriptor;
-	int i, y;
+	int fileDescriptor, i, y;
 	char *buffer;
 
 	if (!filename)
 		return (0);
-
 	fileDescriptor = open(filename, O_RDONLY);
-
 	if (fileDescriptor < 0)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
-
 	if (!buffer)
 		return (0);
-
 	i = read(fileDescriptor, buffer, letters);
-
 	if (i < 0)
 	{
 		free(buffer);
 		return (0);
 	}
-
 	buffer[i] = '\0';
 	close(fileDescriptor);
-
 	y = write(STDOUT_FILENO, buffer, i);
-
 	if (y < 0)
 	{
 		free(buffer);
 		return (0);
 	}
-
 	free(buffer);
 	return (y);
 }
