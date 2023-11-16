82560fcf0697ce1c916a12387cebaccd20b90035
Hossam Mahmoud <hossam.uddin1@gmail.com>
2023-11-16 20:07:25 +0200

    edited the algorithm for the 3rd task

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ *
+ * @filename: file to append the text to
+ * @text_content: content to append into the file
+ *
+ * Return: 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int bit;
+
+	bit = (n >> index);
+	if (index > 32)
+	return (-1);
+	return (bit & 1);
+}
